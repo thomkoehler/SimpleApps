6c88b6920fceafe1ebfd490b5cd70966bbca4f9a
thomkoehler <thom.koehler@gmail.com>
2013-10-17 16:56:33 +0200

    change FindFile

--- src/FindFile.hs
+++ src/FindFile.hs
@@ -10,17 +10,26 @@ import Control.Monad.Trans.Resource(runResourceT, allocate, release)
 import Control.Monad.Trans.Class(lift)
 import System.IO(openFile, hClose, IOMode(ReadMode), hIsEOF, hSeek, SeekMode(RelativeSeek))
 import Data.ByteString.Search(breakOn)
-import Control.Monad(forM_, forM)
+import Control.Monad(forM_, when)
 import System.Directory(getDirectoryContents, doesDirectoryExist)
 import System.FilePath((</>))
 
 ------------------------------------------------------------------------------------------------------------------------
 
+currBufferSize :: Int
+currBufferSize = 10240
+
+------------------------------------------------------------------------------------------------------------------------
+
 main :: IO ()
 main = do
-   files <- listDir "..//" (textFindInFile "class" 10240) True
-   forM_ files putStrLn
+   foreachFile "..//" (fileFun "class") True
    return ()  
+   where
+      fileFun :: B.ByteString -> FilePath -> IO ()
+      fileFun text fileName = do
+         found <- textFindInFile text currBufferSize fileName
+         when found $ putStrLn fileName
 
 
 textFindInFile :: B.ByteString -> Int -> FilePath -> IO Bool 
@@ -50,20 +59,15 @@ textFindInFile text bufferSize fileName = runResourceT $ do
                   return True
      
       
-listDir :: FilePath -> (FilePath -> IO Bool) -> Bool -> IO [FilePath]
-listDir topdir fileSelect recursive = do
+foreachFile :: FilePath -> (FilePath -> IO ()) -> Bool -> IO ()
+foreachFile topdir fileFun recursive = do
    names <- getDirectoryContents topdir
    let properNames = filter (`notElem` [".", ".."]) names
-   paths <- forM properNames $ \name -> do
+   forM_ properNames $ \name -> do
       let path = topdir </> name
       isDirectory <- doesDirectoryExist path
       if isDirectory && recursive
-         then listDir path fileSelect True
-         else do
-            res <- fileSelect path
-            if res
-               then return [path]
-               else return []
-   return (concat paths)
+         then foreachFile path fileFun True
+         else fileFun path
       
 ------------------------------------------------------------------------------------------------------------------------
