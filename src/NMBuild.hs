b6daee105862683b65dfd4b1980e84a78e21a361
thomkoehler <thom.koehler@gmail.com>
2013-11-15 15:27:39 +0100

    add NMBuild

---
+++ src/NMBuild.hs
@@ -0,0 +1,76 @@
+
+------------------------------------------------------------------------------------------------------------------------
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module Main where
+
+import Prelude hiding(FilePath)
+import Shelly
+import Data.Maybe
+import Data.Text.Lazy hiding(map)
+import Control.Monad
+import System.Environment(getArgs)
+
+------------------------------------------------------------------------------------------------------------------------
+
+project :: FilePath
+project = "NetworkManager"
+
+
+buildDefs :: [Text]
+buildDefs =
+   [
+      "Version",
+      "Helper",
+      "NMDBServerBase",
+      "NMDBA",
+      "NMDBServer.Net",
+      "TextResource",
+      "ProcessMonitor",
+      "NMDBServer",
+      "NMTextCreator",
+      "ProcessAdmin",
+--      "ReportingtypeCompression",
+      "NMDBCOMServiceCenterNMCopy",
+--      "CopyRTL",
+      "CommonTools",
+--      "EventServerClient",
+--      "Scheduler",
+--      "tGProtocolLib",
+      "nmstring",
+      "SharedBuffer",
+      "SharedBufferExtension",
+      "NMDBServerAssistantLayer"
+   ]
+
+
+main :: IO ()
+main = shelly $ do
+   ver <- getEnv "ver" "dev"
+   tfs <- getEnv "tfs" "D:/Projects"
+   
+   args <- liftIO getArgs
+   let targs = map pack args
+   
+   let buildDir = tfs </> project </> ver
+   cd buildDir
+   
+   forM_ buildDefs $ build targs
+   return ()
+
+------------------------------------------------------------------------------------------------------------------------
+
+build :: [Text] -> Text -> Sh ()
+build args buildDef = do
+   run_ "build.bat" $ (toStrict buildDef) : (map toStrict args) 
+
+  
+getEnv :: Text -> Text -> Sh String
+getEnv name def = do
+   maybeValue <- get_env $ toStrict name
+   return $ unpack $ fromStrict $ fromMaybe (toStrict def) maybeValue
+   
+------------------------------------------------------------------------------------------------------------------------
