cb320f2f951b713c7182ef4658c8ccab7992ab70
thomkoehler <thom.koehler@gmail.com>
2013-12-09 12:55:34 +0100

    bugfixing

--- src/NMBuild.hs
+++ src/NMBuild.hs
@@ -7,12 +7,14 @@ import Control.Monad
 import System.Environment(getArgs)
 import System.FilePath
 import Control.Monad.IO.Class
+import Text.Printf
 
 ------------------------------------------------------------------------------------------------------------------------
 
 project :: FilePath
 project = "NetworkManager"
 
+installDir = "D:\\NMInstall_%s_%s"
 
 buildDefs :: [String]
 buildDefs =
@@ -40,14 +42,20 @@ buildDefs =
       "NMDBServerAssistantLayer"
    ]
 
-
+  
 main :: IO ()
 main = shell $ do
    ver <- getEnv_ "ver" "dev"
    tfs <- getEnv_ "tfs" "D:/Projects"
-   
+
    args <- liftIO getArgs
+   let po = poFromArguments args
    
+   setEnv "NM_INSTALL" $ printf installDir (show (poPlatform po)) (show (poConfig po))
+
+--   echo "ver: $ver"
+--   echo "tfs: $tfs"
+--   echo "NM_INSTALL: $NM_INSTALL"
   
    let buildDir = tfs </> project </> ver
    cd buildDir
@@ -62,4 +70,27 @@ build args buildDef = do
    _ <- sh "build.bat" $ buildDef : args
    return ()  
 
+data Config = Release | Debug deriving Show
+
+data Platform = X86 | X64 deriving Show
+   
+data ProgramOptions = ProgramOptions
+   {
+      poConfig :: Config,
+      poPlatform :: Platform 
+   }
+   deriving Show
+
+   
+poFromArguments :: [String] -> ProgramOptions
+poFromArguments args = foldl fun defPO args
+   where
+      defPO = ProgramOptions Release X86
+      fun po arg =
+         case arg of
+            "-dbg" -> po { poConfig = Debug }
+            "-x64" -> po { poPlatform = X64 }
+            _      -> po
+
+  
 ------------------------------------------------------------------------------------------------------------------------
