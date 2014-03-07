7eff92ce6f4c9e21d5139f523044ae22eab5526d
thomkoehler <thom.koehler@gmail.com>
2014-03-07 14:07:03 +0100

    BuildLogInfo

--- src/BuildLogInfo.hs
+++ src/BuildLogInfo.hs
@@ -3,7 +3,7 @@
 
 module Main where
 
-import Prelude hiding(map, lines)
+import Prelude hiding(map, lines, unlines)
 import Data.Conduit(runResourceT, ($$), (=$), (=$=), Conduit)
 import Data.Conduit.Binary(sourceFile, sinkHandle, lines)
 import Data.Conduit.List(map, concatMapAccum)
@@ -11,7 +11,6 @@ import System.IO(stdout)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as C
 import Text.Regex.Posix
-import Text.Regex.Base.RegexLike
 
 -----------------------------------------------------------------------------------------------------------------------
 
@@ -32,27 +31,39 @@ main :: IO ()
 main = runResourceT $ 
    sourceFile "build_log_7.0.txt" 
    $$ lines 
-   =$ (parseBuildLogToken  =$= map (C.pack . show))
+   =$ (parseBuildLogToken  =$= map (C.pack . show)) =$= lines'
    =$ sinkHandle stdout
+   where
+      lines' = map (C.append (C.pack "\n"))
 
+  
 
 parseBuildLogToken :: Monad m => Conduit B.ByteString m BuildLogToken
 parseBuildLogToken = concatMapAccum step ()
    where
       step line _ = 
-         case parseLine line of
+         case  regexParser regexs line of
             BuildStart name -> ((), [BuildStart name])
             _               -> ((), [])
             
 
+regexs = 
+   [
+      ("^------ Build started: Project: ([a-zA-z]*)", BuildStart),
+      ("^\\*\\*\\*  Building ([a-zA-z]*)", BuildStart)
+   ]
 
-parseLine :: B.ByteString -> BuildLogToken
-parseLine line =
-   --case line =~ "------ Build started: Project: ([a-zA-z]*)" of
-   case matchOnce  regexBuildStarted line of
-      --[_ : projectName : _] -> BuildStart projectName
-      Just x -> BuildStart $ C.pack $ show x
-      Nothing -> None
 
+regexParser funs line = foldl step None funs
+   where
+      step prevToken (regex, createFun) = 
+         case prevToken of
+            None -> 
+               case line =~ regex of
+                  [_ : name : _] -> createFun name
+                  _              -> None
+               
+            _    -> prevToken
+         
 
 -----------------------------------------------------------------------------------------------------------------------
