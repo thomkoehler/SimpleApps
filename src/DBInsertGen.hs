978fd74db9e5948d562dbcd4b25493a1d91fc194
thomkoehler <thom.koehler@gmx.com>
2014-07-02 09:21:43 +0200

    [changed] DBInsertGen

--- src/DBInsertGen.hs
+++ src/DBInsertGen.hs
@@ -1,4 +1,6 @@
 
+-----------------------------------------------------------------------------------------------------------------------
+
 module DBInsertGen where
 
 import Database.HDBC.ODBC(connectODBC)
@@ -7,43 +9,81 @@ import System.Environment(getArgs)
 import Data.List(intercalate)
 import Control.Monad(forM_)
 import Data.ByteString.Char8(unpack)
+import qualified Data.ByteString as BS
 
+-----------------------------------------------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
-   --(conn : _) <- getArgs
-   let conn = "DSN=Main"
+   args <- getArgs
+   if length args /= 2
+      then usage
+      else do
+         (conn : tableName : _) <- getArgs
+         dbConnection <- connectODBC conn
+         printInserts dbConnection tableName
    
-   dbConnection <- connectODBC conn
-   printInserts dbConnection "ErmTreeentry"
-   putStrLn ""
-   printInserts dbConnection "ErmTree"
-   putStrLn "" 
    
+usage :: IO ()
+usage = do
+   putStrLn "usage:"
+   putStrLn "   DBInsertGen \"DSN=<name>\" <table-name>"
+
    
 printInserts dbConnection tableName = do    
    tableDesc <- describeTable dbConnection tableName
    let 
-      sqlSelect = createSelect tableName tableDesc
-      insertPrefix = "INSERT INTO " ++ tableName ++ "(" ++ intercalate ", " (map fst tableDesc) ++ ") VALUES("
-      colDescs = map snd tableDesc
+      colNames = map fst tableDesc
+      sqlSelect = createSelect tableName colNames
    
    results <- quickQuery' dbConnection sqlSelect []
-   
    forM_ results $ \result -> do
-      putStrLn $ showRow insertPrefix colDescs result  
+      printInsertPrefix tableName colNames result
+      putStr " VALUES("
+      printRowValues result  
+      putStrLn ");"
     
    return ()
    
    
-createSelect :: String -> [(String, SqlColDesc)] -> String
-createSelect tableName tableDesc = "SELECT " ++ (intercalate ", " (map fst tableDesc)) ++ " FROM " ++ tableName
+createSelect :: String -> [String] -> String
+createSelect tableName colNames = "SELECT " ++ (intercalate ", " colNames) ++ " FROM " ++ tableName
 
 
-showRow :: String -> [SqlColDesc] -> [SqlValue] -> String
-showRow insertPrefix _ values = insertPrefix ++ intercalate ", " (map showValue values) ++ ");"
+printInsertPrefix :: String -> [String] -> [SqlValue] -> IO ()
+printInsertPrefix tableName colNames values = do
+   putStr "INSERT INTO "
+   putStr tableName
+   putStr "("
+   printRowNames (filter notNull (zip colNames values))
+   putStr ")"
+   
    where
-      showValue (SqlString str) = "'" ++ str ++ "'"
-      showValue (SqlByteString str) = "'" ++ unpack str ++ "'"
-      showValue (SqlInt32 i) = show i
-      showValue x = show x
+      notNull (_, SqlNull) = False
+      notNull _ = True
+   
+      printRowNames [] = return ()
+      printRowNames ((n, _) : ns) = do
+         putStr n
+         forM_ ns $ \(n, _) -> putStr ", " >> putStr n
+    
+      
+printRowValues :: [SqlValue] -> IO ()
+printRowValues [] = return ()
+printRowValues values = printRowValues' (filter notNull values)  
+   where
+      notNull SqlNull = False
+      notNull _ = True
+   
+      printRowValues' [] = return () 
+      printRowValues' (v : vs) = do
+         printValue v
+         forM_ vs $ \value -> putStr ", " >> printValue value
+   
+      printValue (SqlString str) = putStr "'" >> putStr str >> putStr "'"
+      printValue (SqlByteString str) = putStr "'" >> BS.putStr str >> putStr "'"
+      printValue (SqlInt32 i) = putStr $ show i 
+      printValue (SqlChar c) = putStr $ show $ fromEnum c
+      printValue x = putStr $ show x
+   
+-----------------------------------------------------------------------------------------------------------------------
