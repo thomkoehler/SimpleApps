d93c713358d1bd6233c4d797dcb04b15ab3e932a
thomkoehler <thom.koehler@gmx.com>
2014-07-03 15:18:06 +0200

    DBInsertGen

--- src/DBInsertGen.hs
+++ src/DBInsertGen.hs
@@ -10,6 +10,10 @@ import Data.List(intercalate)
 import Control.Monad(forM_)
 import Data.ByteString.Char8(unpack)
 import qualified Data.ByteString as BS
+import Data.Time.LocalTime
+import Data.Time.Calendar
+import Text.Printf
+import Data.Fixed
 
 -----------------------------------------------------------------------------------------------------------------------
 
@@ -19,7 +23,7 @@ main = do
    if length args /= 2
       then usage
       else do
-         (conn : tableName : _) <- getArgs
+         let (conn : tableName : _) = args
          dbConnection <- connectODBC conn
          printInserts dbConnection tableName
    
@@ -30,6 +34,7 @@ usage = do
    putStrLn "   DBInsertGen \"DSN=<name>\" <table-name>"
 
    
+printInserts :: IConnection conn => conn -> String -> IO ()
 printInserts dbConnection tableName = do    
    tableDesc <- describeTable dbConnection tableName
    let 
@@ -47,7 +52,7 @@ printInserts dbConnection tableName = do
    
    
 createSelect :: String -> [String] -> String
-createSelect tableName colNames = "SELECT " ++ (intercalate ", " colNames) ++ " FROM " ++ tableName
+createSelect tableName colNames = "SELECT " ++ intercalate ", " colNames ++ " FROM " ++ tableName
 
 
 printInsertPrefix :: String -> [String] -> [SqlValue] -> IO ()
@@ -55,35 +60,42 @@ printInsertPrefix tableName colNames values = do
    putStr "INSERT INTO "
    putStr tableName
    putStr "("
-   printRowNames (filter notNull (zip colNames values))
+   printIntercalate (putStr ", " ) (putStr . fst) $ filter notNull (zip colNames values)
    putStr ")"
    
    where
       notNull (_, SqlNull) = False
       notNull _ = True
-   
-      printRowNames [] = return ()
-      printRowNames ((n, _) : ns) = do
-         putStr n
-         forM_ ns $ \(n, _) -> putStr ", " >> putStr n
     
       
 printRowValues :: [SqlValue] -> IO ()
 printRowValues [] = return ()
-printRowValues values = printRowValues' (filter notNull values)  
+printRowValues values = printIntercalate (putStr ", ") printValue  $ filter notNull values  
    where
       notNull SqlNull = False
       notNull _ = True
-   
-      printRowValues' [] = return () 
-      printRowValues' (v : vs) = do
-         printValue v
-         forM_ vs $ \value -> putStr ", " >> printValue value
    
       printValue (SqlString str) = putStr "'" >> putStr str >> putStr "'"
       printValue (SqlByteString str) = putStr "'" >> BS.putStr str >> putStr "'"
       printValue (SqlInt32 i) = putStr $ show i 
       printValue (SqlChar c) = putStr $ show $ fromEnum c
-      printValue x = putStr $ show x
+      printValue (SqlLocalTime lt) = printLocalTime lt
+      printValue x = error $ "\nValue type " ++ show x ++ " not yet supported."
+   
+   
+printIntercalate :: Monad m => m () -> (a -> m ()) -> [a] -> m ()
+printIntercalate intercalateFun printFun [] = return () 
+printIntercalate intercalateFun printFun (x : xs) = do
+   printFun x
+   forM_ xs $ \x -> intercalateFun >> printFun x    
+
+
+printLocalTime :: LocalTime -> IO ()
+printLocalTime (LocalTime ltDay ltTimeOfDay) = do
+   let 
+      (year, month, day) = toGregorian ltDay
+      (TimeOfDay hour min sec) = ltTimeOfDay
+   printf "CONVERT (DATETIME, '%04d-%02d-%02d %02d:%02d:%s', 21)" year month day hour min (showFixed True sec)
+
    
 -----------------------------------------------------------------------------------------------------------------------
